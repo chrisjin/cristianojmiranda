69461581edf2c6924c2c503f76535fd0c33621ec
cristianojmiranda <cristianojmiranda@4f04c177-d581-9615-3489-ce1901f2c19e>
2010-04-17 14:06:01 +0000

    Objetos versionados indevidamente

--- unicamp/mc326/lab02/Debug/src/hashmap.d
+++
@@ -1,3 +0,0 @@
-src/hashmap.d src/hashmap.o: ../src/hashmap.c ../src/hashmap.h
-
-../src/hashmap.h:
--- unicamp/mc326/lab02/Debug/src/utils.d
+++
@@ -1,5 +0,0 @@
-src/utils.d src/utils.o: ../src/utils.c ../src/log.h ../src/utils.h
-
-../src/log.h:
-
-../src/utils.h:
