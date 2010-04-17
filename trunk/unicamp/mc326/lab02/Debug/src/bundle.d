dc42db91d2fa0f6bb601aa8c4fe5760af84e2dfb
cristianojmiranda <cristianojmiranda@4f04c177-d581-9615-3489-ce1901f2c19e>
2010-04-17 14:06:01 +0000

    Objetos versionados indevidamente

--- trunk/unicamp/mc326/lab02/Debug/src/bundle.d
+++
@@ -1,8 +0,0 @@
-src/bundle.d src/bundle.o: ../src/bundle.c ../src/utils.h \
- ../src/hashmap.h ../src/bundle.h
-
-../src/utils.h:
-
-../src/hashmap.h:
-
-../src/bundle.h:
--- trunk/unicamp/mc326/lab02/Debug/src/log.d
+++
@@ -1,8 +0,0 @@
-src/log.d src/log.o: ../src/log.c ../src/bundle.h ../src/hashmap.h \
- ../src/log.h
-
-../src/bundle.h:
-
-../src/hashmap.h:
-
-../src/log.h:
