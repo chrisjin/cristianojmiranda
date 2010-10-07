a26e40dee974302f9ce2db0447f33e77aa896be6
cristianojmiranda <cristianojmiranda@4f04c177-d581-9615-3489-ce1901f2c19e>
2010-10-07 19:36:31 +0000

    Script de geração do banco apartir do banco gerado pelo spring roo

--- unicamp/mc536/Projeto 1/Arquitetura/Data Model/Create Data Base Script.sql
+++ unicamp/mc536/Projeto 1/Arquitetura/Data Model/Create Data Base Script.sql
@@ -1,219 +1,256 @@
-CREATE TABLE TipoDocumento (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nm_tipo_documento VARCHAR(255) NOT NULL,
-  ds_tipo_documento VARCHAR(255) NOT NULL,
-  ds_detalhada_tipo_documento TEXT NULL,
-  PRIMARY KEY(id)
-);
+-- MySQL dump 10.9
+--
+-- Host: localhost    Database: forum
+-- ------------------------------------------------------
+-- Server version	4.1.18-nt
 
-CREATE TABLE Pais (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  nm_pais VARCHAR(50) NOT NULL,
-  ds_pais TEXT NULL,
-  PRIMARY KEY(id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE Lingua (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  id_pais INTEGER UNSIGNED NOT NULL,
-  nm_lingua VARCHAR(50) NOT NULL,
-  nm_regiao VARCHAR(255) NOT NULL,
-  ds_lingua VARCHAR(255) NOT NULL,
-  ds_detalhada_lingua TEXT NULL,
-  PRIMARY KEY(id),
-  INDEX Lingua_FKIndex1(id_pais),
-  FOREIGN KEY(id_pais)
-    REFERENCES Pais(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+--
+-- Table structure for table `comentario_documento`
+--
 
-CREATE TABLE Usuario (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  id_lingua INTEGER UNSIGNED NOT NULL,
-  ds_login VARCHAR(10) NOT NULL,
-  ds_senha VARCHAR(10) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  nm_usuario VARCHAR(255) NULL,
-  tp_usuario_acesso INTEGER UNSIGNED NOT NULL DEFAULT 3,
-  nm_instituicao VARCHAR(255) NULL,
-  tp_usuario CHAR(1) NOT NULL DEFAULT U,
-  fl_ativo INTEGER UNSIGNED NOT NULL DEFAULT S,
-  PRIMARY KEY(id),
-  INDEX Usuario_FKIndex1(id_lingua),
-  FOREIGN KEY(id_lingua)
-    REFERENCES Lingua(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+DROP TABLE IF EXISTS `comentario_documento`;
+CREATE TABLE `comentario_documento` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `ds_comentario` varchar(255) NOT NULL default '',
+  `dt_inclusao` datetime NOT NULL default '0000-00-00 00:00:00',
+  `tp_referencia_documento` int(11) NOT NULL default '0',
+  `version` int(11) default NULL,
+  `id_comentario` bigint(20) default NULL,
+  `id_documento` bigint(20) NOT NULL default '0',
+  `id_usuario` bigint(20) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `FK4B988FD64580C13D` (`id_documento`),
+  KEY `FK4B988FD6F63EF371` (`id_usuario`),
+  KEY `FK4B988FD6F6F17011` (`id_comentario`),
+  CONSTRAINT `FK4B988FD6F6F17011` FOREIGN KEY (`id_comentario`) REFERENCES `comentario_documento` (`id`),
+  CONSTRAINT `FK4B988FD64580C13D` FOREIGN KEY (`id_documento`) REFERENCES `documento` (`id`),
+  CONSTRAINT `FK4B988FD6F63EF371` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE Projeto (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  id_usuario_responsavel INTEGER UNSIGNED NOT NULL,
-  nm_projeto VARCHAR(255) NOT NULL,
-  ds_projeto VARCHAR(255) NOT NULL,
-  ds_detalhada_projeto TEXT NULL,
-  dt_inicio DATETIME NOT NULL,
-  PRIMARY KEY(id),
-  INDEX Projeto_FKIndex1(id_usuario_responsavel),
-  FOREIGN KEY(id_usuario_responsavel)
-    REFERENCES Usuario(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+--
+-- Table structure for table `descricao_documento`
+--
 
-CREATE TABLE ParticipantesProjeto (
-  id_usuario INTEGER UNSIGNED NOT NULL,
-  id_projeto INTEGER UNSIGNED NOT NULL,
-  PRIMARY KEY(id_usuario, id_projeto),
-  INDEX Usuario_has_Projeto_FKIndex1(id_usuario),
-  INDEX Usuario_has_Projeto_FKIndex2(id_projeto),
-  FOREIGN KEY(id_usuario)
-    REFERENCES Usuario(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(id_projeto)
-    REFERENCES Projeto(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+DROP TABLE IF EXISTS `descricao_documento`;
+CREATE TABLE `descricao_documento` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `ds_documento` varchar(255) NOT NULL default '',
+  `version` int(11) default NULL,
+  `id_documento` bigint(20) NOT NULL default '0',
+  `id_idioma_documento` bigint(20) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `FK9923EC3E25A527EA` (`id_idioma_documento`),
+  KEY `FK9923EC3E4580C13D` (`id_documento`),
+  CONSTRAINT `FK9923EC3E4580C13D` FOREIGN KEY (`id_documento`) REFERENCES `documento` (`id`),
+  CONSTRAINT `FK9923EC3E25A527EA` FOREIGN KEY (`id_idioma_documento`) REFERENCES `idioma` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE Documento (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  id_projeto INTEGER UNSIGNED NOT NULL,
-  id_lingua INTEGER UNSIGNED NOT NULL,
-  id_tipo_documento INTEGER UNSIGNED NOT NULL,
-  id_usuario_autor INTEGER UNSIGNED NULL,
-  id_usuario_responsavel INTEGER UNSIGNED NULL,
-  nm_documento VARCHAR(255) NOT NULL,
-  documento BLOB NOT NULL,
-  dt_criacao DATETIME NOT NULL,
-  dt_inclusao DATETIME NOT NULL,
-  ds_documento VARCHAR(255) NULL,
-  nm_arquivo VARCHAR(255) NULL,
-  nm_autor VARCHAR(255) NULL,
-  PRIMARY KEY(id),
-  INDEX Documento_FKIndex1(id_tipo_documento),
-  INDEX Documento_FKIndex2(id_usuario_autor),
-  INDEX Documento_FKIndex3(id_usuario_responsavel),
-  INDEX Documento_FKIndex4(id_lingua),
-  INDEX Documento_FKIndex5(id_projeto),
-  FOREIGN KEY(id_tipo_documento)
-    REFERENCES TipoDocumento(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(id_usuario_autor)
-    REFERENCES Usuario(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(id_usuario_responsavel)
-    REFERENCES Usuario(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(id_lingua)
-    REFERENCES Lingua(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(id_projeto)
-    REFERENCES Projeto(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+--
+-- Table structure for table `documento`
+--
 
-CREATE TABLE PalavraChave (
-  palavra VARCHAR(255) NOT NULL,
-  id_documento INTEGER UNSIGNED NOT NULL,
-  PRIMARY KEY(palavra),
-  INDEX PalavraChave_FKIndex1(id_documento),
-  FOREIGN KEY(id_documento)
-    REFERENCES Documento(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+DROP TABLE IF EXISTS `documento`;
+CREATE TABLE `documento` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `documento` varchar(255) NOT NULL default '',
+  `ds_documento` text NOT NULL,
+  `ds_email_autor` varchar(100) default NULL,
+  `dt_criacao` datetime NOT NULL default '0000-00-00 00:00:00',
+  `dt_inclusao` datetime NOT NULL default '0000-00-00 00:00:00',
+  `nm_arquivo` varchar(255) NOT NULL default '',
+  `nm_autor` varchar(255) default NULL,
+  `nm_documento` varchar(255) NOT NULL default '',
+  `qtd_visualizacao` bigint(20) NOT NULL default '0',
+  `version` int(11) default NULL,
+  `id_idioma_documento` bigint(20) NOT NULL default '0',
+  `id_programa` bigint(20) default NULL,
+  `id_usuario_autor` bigint(20) default NULL,
+  `id_usuario_responsavel` bigint(20) default NULL,
+  `tipo_documento` bigint(20) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `FK383D52B4603D80F2` (`id_usuario_responsavel`),
+  KEY `FK383D52B425A527EA` (`id_idioma_documento`),
+  KEY `FK383D52B410F27F95` (`id_usuario_autor`),
+  KEY `FK383D52B4EDD17EC2` (`tipo_documento`),
+  KEY `FK383D52B44535FA45` (`id_programa`),
+  CONSTRAINT `FK383D52B44535FA45` FOREIGN KEY (`id_programa`) REFERENCES `programa` (`id`),
+  CONSTRAINT `FK383D52B410F27F95` FOREIGN KEY (`id_usuario_autor`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `FK383D52B425A527EA` FOREIGN KEY (`id_idioma_documento`) REFERENCES `idioma` (`id`),
+  CONSTRAINT `FK383D52B4603D80F2` FOREIGN KEY (`id_usuario_responsavel`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `FK383D52B4EDD17EC2` FOREIGN KEY (`tipo_documento`) REFERENCES `tipo_documento` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE TraducaoDocumento (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  id_usuario INTEGER UNSIGNED NOT NULL,
-  id_documento INTEGER UNSIGNED NOT NULL,
-  ds_traducao VARCHAR(255) NOT NULL,
-  traducao TEXT NOT NULL,
-  dt_traducao DATETIME NOT NULL,
-  PRIMARY KEY(id),
-  INDEX TraducaoDocumento_FKIndex1(id_usuario),
-  INDEX TraducaoDocumento_FKIndex2(id_documento),
-  FOREIGN KEY(id_usuario)
-    REFERENCES Usuario(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(id_documento)
-    REFERENCES Documento(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+--
+-- Table structure for table `idioma`
+--
 
-CREATE TABLE UsuarioDocumentoInteresse (
-  id_usuario INTEGER UNSIGNED NOT NULL,
-  id_documento INTEGER UNSIGNED NOT NULL,
-  PRIMARY KEY(id_usuario, id_documento),
-  INDEX Usuario_has_Documento_FKIndex1(id_usuario),
-  INDEX Usuario_has_Documento_FKIndex2(id_documento),
-  FOREIGN KEY(id_usuario)
-    REFERENCES Usuario(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(id_documento)
-    REFERENCES Documento(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+DROP TABLE IF EXISTS `idioma`;
+CREATE TABLE `idioma` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `ds_detalhada_idioma` varchar(255) default NULL,
+  `ds_idioma` varchar(255) NOT NULL default '',
+  `nm_idioma` varchar(50) NOT NULL default '',
+  `nm_regiao` varchar(255) default NULL,
+  `sg_idioma` varchar(4) NOT NULL default '',
+  `version` int(11) default NULL,
+  `id_pais` bigint(20) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `FKB8DF70D5C21BD8C1` (`id_pais`),
+  CONSTRAINT `FKB8DF70D5C21BD8C1` FOREIGN KEY (`id_pais`) REFERENCES `pais` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE DocumentoComentario (
-  id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  id_usuario INTEGER UNSIGNED NULL,
-  id_comentario_pai INTEGER UNSIGNED NULL,
-  id_documento INTEGER UNSIGNED NULL,
-  ds_comentario TEXT NULL,
-  dt_post DATETIME NULL,
-  fl_ativo CHAR(1) NULL DEFAULT S,
-  PRIMARY KEY(id),
-  INDEX DocumentoComentario_FKIndex1(id_documento),
-  INDEX DocumentoComentario_FKIndex2(id_comentario_pai),
-  INDEX DocumentoComentario_FKIndex3(id_usuario),
-  FOREIGN KEY(id_documento)
-    REFERENCES Documento(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(id_comentario_pai)
-    REFERENCES DocumentoComentario(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(id_usuario)
-    REFERENCES Usuario(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+--
+-- Table structure for table `pais`
+--
 
-CREATE TABLE DocumentoRelacionado (
-  id_documento1 INTEGER UNSIGNED NOT NULL,
-  id_documento2 INTEGER UNSIGNED NOT NULL,
-  id_usuario INTEGER UNSIGNED NOT NULL,
-  ds_motivo TEXT NOT NULL,
-  PRIMARY KEY(id_documento1, id_documento2),
-  INDEX Documento_has_Documento_FKIndex1(id_documento1),
-  INDEX Documento_has_Documento_FKIndex2(id_documento2),
-  INDEX DocumentoRelacionado_FKIndex3(id_usuario),
-  FOREIGN KEY(id_documento1)
-    REFERENCES Documento(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(id_documento2)
-    REFERENCES Documento(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-  FOREIGN KEY(id_usuario)
-    REFERENCES Usuario(id)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION
-);
+DROP TABLE IF EXISTS `pais`;
+CREATE TABLE `pais` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `ds_pais` varchar(255) NOT NULL default '',
+  `nm_pais` varchar(50) NOT NULL default '',
+  `version` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Table structure for table `palavra_documento`
+--
+
+DROP TABLE IF EXISTS `palavra_documento`;
+CREATE TABLE `palavra_documento` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `ds_palavras_chaves` varchar(200) NOT NULL default '',
+  `version` int(11) default NULL,
+  `id_documento` bigint(20) NOT NULL default '0',
+  `id_idioma` bigint(20) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `FKE7D716B44580C13D` (`id_documento`),
+  KEY `FKE7D716B492F95DF5` (`id_idioma`),
+  CONSTRAINT `FKE7D716B492F95DF5` FOREIGN KEY (`id_idioma`) REFERENCES `idioma` (`id`),
+  CONSTRAINT `FKE7D716B44580C13D` FOREIGN KEY (`id_documento`) REFERENCES `documento` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `programa`
+--
+
+DROP TABLE IF EXISTS `programa`;
+CREATE TABLE `programa` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `ds_detalhada_programa` varchar(255) default NULL,
+  `ds_programa` varchar(255) NOT NULL default '',
+  `nm_programa` varchar(255) NOT NULL default '',
+  `version` int(11) default NULL,
+  `id_usuario` bigint(20) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `FKC454B25DF63EF371` (`id_usuario`),
+  CONSTRAINT `FKC454B25DF63EF371` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `rede_trabalho`
+--
+
+DROP TABLE IF EXISTS `rede_trabalho`;
+CREATE TABLE `rede_trabalho` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `ds_detalhado_redetrabalho` varchar(255) default NULL,
+  `ds_redetrabalho` varchar(255) NOT NULL default '',
+  `dt_inclusao` datetime NOT NULL default '0000-00-00 00:00:00',
+  `nm_redetrabalho` varchar(255) NOT NULL default '',
+  `version` int(11) default NULL,
+  `id_usuario` bigint(20) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `FK1ACB4FFCF63EF371` (`id_usuario`),
+  CONSTRAINT `FK1ACB4FFCF63EF371` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `rede_trabalho_usuario`
+--
+
+DROP TABLE IF EXISTS `rede_trabalho_usuario`;
+CREATE TABLE `rede_trabalho_usuario` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `version` int(11) default NULL,
+  `id_rede_trabalho` bigint(20) NOT NULL default '0',
+  `id_usuario` bigint(20) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `FKB60E2DEBF63EF371` (`id_usuario`),
+  KEY `FKB60E2DEB2ACACB64` (`id_rede_trabalho`),
+  CONSTRAINT `FKB60E2DEB2ACACB64` FOREIGN KEY (`id_rede_trabalho`) REFERENCES `rede_trabalho` (`id`),
+  CONSTRAINT `FKB60E2DEBF63EF371` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `tipo_documento`
+--
+
+DROP TABLE IF EXISTS `tipo_documento`;
+CREATE TABLE `tipo_documento` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `ds_detalhado_tipo_documento` varchar(255) default NULL,
+  `ds_tipo_documento` varchar(255) NOT NULL default '',
+  `nm_tipo_documento` varchar(50) NOT NULL default '',
+  `version` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `ds_login` varchar(10) NOT NULL default '',
+  `ds_senha` varchar(10) NOT NULL default '',
+  `dt_inclusao` datetime NOT NULL default '0000-00-00 00:00:00',
+  `email` varchar(255) NOT NULL default '',
+  `fl_ativo` tinyint(1) NOT NULL default '0',
+  `fl_institucional` tinyint(1) NOT NULL default '0',
+  `nm_instituicao` varchar(255) default NULL,
+  `nm_usuario` varchar(255) NOT NULL default '',
+  `tp_usuario` int(11) NOT NULL default '0',
+  `version` int(11) default NULL,
+  `id_idioma` bigint(20) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `FKF814F32E92F95DF5` (`id_idioma`),
+  CONSTRAINT `FKF814F32E92F95DF5` FOREIGN KEY (`id_idioma`) REFERENCES `idioma` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `usuario_documento`
+--
+
+DROP TABLE IF EXISTS `usuario_documento`;
+CREATE TABLE `usuario_documento` (
+  `id` bigint(20) NOT NULL auto_increment,
+  `dt_inclusao` datetime NOT NULL default '0000-00-00 00:00:00',
+  `version` int(11) default NULL,
+  `id_documento` bigint(20) NOT NULL default '0',
+  `id_usuario` bigint(20) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  KEY `FKA5579EE34580C13D` (`id_documento`),
+  KEY `FKA5579EE3F63EF371` (`id_usuario`),
+  CONSTRAINT `FKA5579EE3F63EF371` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `FKA5579EE34580C13D` FOREIGN KEY (`id_documento`) REFERENCES `documento` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
