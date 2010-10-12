dec3402c1ed9c60d67dcf5bf88cd8cef43077705
cristianojmiranda <cristianojmiranda@4f04c177-d581-9615-3489-ce1901f2c19e>
2010-10-12 00:43:19 +0000

    Configuração do ambiente de desenvolvimento, tela de login, crud de tipo de documento

---
+++ unicamp/mc536/Projeto 1/Source/forum/src/main/java/br/unicamp/mc536/t2010s2a/forum/utils/Constantes.java
@@ -0,0 +1,31 @@
+package br.unicamp.mc536.t2010s2a.forum.utils;
+
+/**
+ * Classe com as constantes genericas da aplicação.
+ * 
+ * @author Cristiano
+ * 
+ */
+public class Constantes {
+
+	/**
+	 * Usuario da sessão
+	 */
+	public static final String SESSION_USER = "SESSION_USER";
+
+	/**
+	 * Mensagem vazia
+	 */
+	public static final String MSG_EMPTY = "msg.empty";
+
+	/**
+	 * Mensagem para login e senha invalido
+	 */
+	public static final String MSG_LOGIN_INVALIDO = "msg.login.usario_senha_invalido";
+
+	/**
+	 * Mensagem para campo obrigatorio não preenchido.
+	 */
+	public static final String MSG_CAMPO_OBRIGATORIO_NAO_PREENCHIDO = "msg.generic.campos_obrigatorios_nao_preenchidos";
+
+}
