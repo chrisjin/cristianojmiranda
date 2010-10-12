1aaf03edcd4bd05a0cd54f8a0d362434d987e022
cristianojmiranda <cristianojmiranda@4f04c177-d581-9615-3489-ce1901f2c19e>
2010-10-12 15:50:26 +0000

    Ajax

--- trunk/unicamp/mc536/Projeto 1/Source/forum/src/main/java/br/unicamp/mc536/t2010s2a/forum/web/dwr/UsuarioDWR.java
+++ trunk/unicamp/mc536/Projeto 1/Source/forum/src/main/java/br/unicamp/mc536/t2010s2a/forum/web/dwr/UsuarioDWR.java
@@ -1,10 +1,28 @@
 package br.unicamp.mc536.t2010s2a.forum.web.dwr;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.ServletException;
+
+import org.directwebremoting.WebContextFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 import br.unicamp.mc536.t2010s2a.forum.domain.Usuario;
 
 public class UsuarioDWR {
+
+	@Autowired
+	private ReloadableResourceBundleMessageSource bundle;
+
+	public ReloadableResourceBundleMessageSource getBundle() {
+		return bundle;
+	}
+
+	public void setBundle(ReloadableResourceBundleMessageSource bundle) {
+		this.bundle = bundle;
+	}
 
 	/**
 	 * Consulta todos os usuarios
@@ -14,6 +32,48 @@ public class UsuarioDWR {
 	public List<Usuario> listarUsuarios() {
 
 		return Usuario.findAllUsuarios();
+	}
+
+	/**
+	 * 
+	 * Consulta de usuarios com resultado para injetar na tela de busca.
+	 * 
+	 * @return
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	public String listarUsuariosInject() throws ServletException, IOException {
+
+		String result = "";
+
+		// Obtem todos os usuarios
+		List<Usuario> usuarios = Usuario.findAllUsuarios();
+
+		if (usuarios != null && !usuarios.isEmpty()) {
+
+			// Coloca a lista de usuarios na request
+			WebContextFactory.get().getHttpServletRequest().setAttribute(
+					"usuarios", usuarios);
+
+			/*
+			 * WebContextFactory .get() .getHttpServletRequest() .setAttribute(
+			 * "labelNome", this.bundle .getMessage(
+			 * "label_br_unicamp_mc536_t2010s2a_forum_domain_usuario_nmusuario",
+			 * null, RequestContextUtils .getLocale(WebContextFactory .get()
+			 * .getHttpServletRequest())));
+			 */
+
+			WebContextFactory.get().getHttpServletRequest().setAttribute(
+					"labelNome", "Nome");
+
+			// Processa a pagina de usuarios
+			result = WebContextFactory.get().forwardToString(
+					"/WEB-INF/views/usuarios/dwrListUsuarios.jspx");
+
+		}
+
+		return result;
+
 	}
 
 }
