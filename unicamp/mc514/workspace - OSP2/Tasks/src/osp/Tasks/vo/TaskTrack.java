a74e02f1bcc045a4a4435248929428569e22b352
cristianojmiranda <cristianojmiranda@gmail.com>
2011-09-22 22:53:25 +0000

    osp2

---
+++ unicamp/mc514/workspace - OSP2/Tasks/src/osp/Tasks/vo/TaskTrack.java
@@ -0,0 +1,28 @@
+package osp.Tasks.vo;
+
+import osp.FileSys.OpenFile;
+import osp.Ports.PortCB;
+import osp.Threads.ThreadCB;
+
+/**
+ * @author ra083382
+ * 
+ */
+public class TaskTrack {
+
+	/**
+	 * 
+	 */
+	private ThreadCB threadCB;
+
+	/**
+	 * 
+	 */
+	private PortCB portCB;
+
+	/**
+	 * 
+	 */
+	private OpenFile openFile;
+
+}
