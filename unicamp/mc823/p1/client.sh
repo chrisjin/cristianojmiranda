5cd1debb93c0c0664a234a1d6e3dcc714b444166
cristianojmiranda <cristianojmiranda@4f04c177-d581-9615-3489-ce1901f2c19e>
2013-04-11 14:43:15 +0000

    commit

--- unicamp/mc823/p1/client.sh
+++ unicamp/mc823/p1/client.sh
@@ -1,2 +1,6 @@
-./mc823-S2013-p1-ra083382 C 12000 143.106.16.163
+# --- inicia o client apontando para a xaveco ---
+./mc823-S2013-p1-ra083382 C 12001 143.106.16.163
+
+# --- inicia o cliente apontado para a rush ---
+#./mc823-S2013-p1-ra083382 C 12001 143.106.16.150
 
